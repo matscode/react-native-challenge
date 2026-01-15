02f5745b9f97a81dcfb704c8e64501ee5e16bd3c
Michael Akanji <matscode@gmail.com>
2026-01-15 14:20:25 +0100

    chore: update app info, icons, and background color

--- app/index.tsx
+++ app/index.tsx
@@ -2,7 +2,7 @@ import { Text, View } from 'react-native';
 
 export default function Page() {
   return (
-    <View className="flex-1 items-center justify-center bg-blue-500">
+    <View className="flex-1 items-center justify-center bg-red-500">
       <Text className="text-white text-xl font-bold">NativeWind is working!</Text>
     </View>
   );
